b00d569ea5abdddffaabd2b35b6bb6ae55a289c4
Nijhuis <nijhp1@bw.local>
2019-08-15 21:21:13 +0200

    added the real files

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = function throwTheDice(maxNumber) {
+    let dice = Math.floor(Math.random() * maxNumber + 1);
+    return dice;
+};
+
+module.exports = function wafwoef() {
+    return('Waf! Woef!');
+};
